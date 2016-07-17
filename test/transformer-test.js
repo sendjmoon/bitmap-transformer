36516ec35024337ffd210d7749c155b9c863d275
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-17 14:47:56 -0700

    basic test using mocha to verify already created files

--- test/transformer-test.js
+++ test/transformer-test.js
@@ -2,35 +2,28 @@ const expect = require('chai').expect;
 const fs = require('fs');
 
 const invertBitmap = require('../lib/invertBitmap');
-const restoreInvertedBitmap = require('../lib/restoreInvertedBitmap');
+const restoreBitmap = require('../lib/restoreBitmap');
 
-describe('create an inverted bitmap of the original', function() {
-  var originalBitmapArray = [];
-  var invertBitmapArray = [];
-  var restoredBitmapArray = [];
+describe('hooks', function() {
+  var fileData = [];
+  var restoredFileData = [];
+
   before(function(done) {
-    fs.readFile('./test/palette-bitmap.bmp', function(err, data) {
+    fs.readFile('../test/palette-bitmap.bmp', function(err, data) {
+      fileData.push(data.toString());
       if(err) console.log(err);
-      originalBitmapArray.push(data.toString());
-      done();
+      fs.readFile('../test/restored-palette-bitmap.bmp', function(err, data) {
+        restoredFileData.push(data.toString());
+        if(err) console.log(err);
+        done();
+      });
     });
   });
 
-  it('the invert bitmap should be output', function(done) {
-    invertBitmap('./test/palette-bitmap.bmp', function(err, data) {
-      if(err) console.log(err);
-      console.log('inverting bitmap');
-      invertBitmapArray.push(data.toString());
-    });
-    done();
-  });
-  
-  it('the restored bitmap should match the original bitmap', function(done) {
-    restoreInvertedBitmap('./test/inverted-palette-bitmap.bmp', function(err, data) {
-      console.log('restoring original bitmap');
-      restoredBitmapArray.push(data.toString());
-      expect(originalBitmapArray).to.eql(restoredBitmapArray);
-    });
+  it('should restore the bitmap', function(done) {
+    expect(fileData.length).to.eql(1);
+    expect(restoredFileData.length).to.eql(1);
+    expect(fileData).to.eql(restoredFileData);
     done();
   });
 });
