e3e56cc66b3ebe5e8cf7771a91ba9a92a445136e
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-17 17:44:59 -0700

    added gulpfile to include tasks to run tests for linter and mocha test on test files

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('mocha');
+
+var allFiles = ['**/*.js', '!./node_modules/**'];
+var testFiles = ['./test/**/*.js'];
+
+gulp.task('lint', () => {
+  return gulp.src(allFiles)
+  .pipe(eslint())
+  .pipe(eslint.format());
+});
+
+gulp.task('test:test', () => {
+  return gulp.src(testFiles)
+  .pipe(mocha());
+});
+
+gulp.task('default', ['lint']);
